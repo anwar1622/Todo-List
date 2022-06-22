31bb3ab10a8eea16a2e431dfd02e4a2f758e1bd5
anwar1622 <anwar16@std.unissula.ac.id>
2022-06-22 16:24:20 +0700

    Updates

---
+++ precache-manifest.a3f2e8c1b0a65faf8f647e347b4e3902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d11d80423d847ffe996",
+    "url": "/Todo-List/static/css/main.3dc13563.chunk.css"
+  },
+  {
+    "revision": "0d11d80423d847ffe996",
+    "url": "/Todo-List/static/js/main.0d11d804.chunk.js"
+  },
+  {
+    "revision": "2782a717d958b3b8bcb6",
+    "url": "/Todo-List/static/js/1.2782a717.chunk.js"
+  },
+  {
+    "revision": "8c9479ccb4ea44ae387b",
+    "url": "/Todo-List/static/js/runtime~main.8c9479cc.js"
+  },
+  {
+    "revision": "1612fb16342979facb8f0a3557a1e05a",
+    "url": "/Todo-List/index.html"
+  }
+];
